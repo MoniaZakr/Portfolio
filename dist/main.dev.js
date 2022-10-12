875069537b6d120d695615782ef1a91f87c50022
Moika Zakrzewska <monikawiniar@gmail.com>
2022-10-12 17:57:27 +0200

    Created a funcion responsible for the operation of navigation

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -4,6 +4,9 @@ var buttons = document.querySelectorAll(".button");
 var aboutSection = document.querySelector(".about_container");
 var homeSection = document.querySelector(".home_container");
 var educationSection = document.querySelector(".education_container");
+var skillsSection = document.querySelector(".skills__Created acontainer");
+var projectsSection = document.querySelector(".projects_container");
+var contactSection = document.querySelector(".contact");
 buttons.forEach(function (button) {
   return button.addEventListener("click", switched);
 });
@@ -16,14 +19,43 @@ function switched(e) {
     homeSection.style.display = "grid";
     aboutSection.style.display = "none";
     educationSection.style.display = "none";
+    skillsSection.style.display = "none";
+    projectsSection.style.display = "none";
+    contactSection.style.display = "none";
   } else if (btn.classList.contains("about")) {
-    homeSection.style.display = "none";
     aboutSection.style.display = "block";
+    homeSection.style.display = "none";
     educationSection.style.display = "none";
+    skillsSection.style.display = "none";
+    projectsSection.style.display = "none";
+    contactSection.style.display = "none";
   } else if (btn.classList.contains("education")) {
+    educationSection.style.display = "flex";
     homeSection.style.display = "none";
     aboutSection.style.display = "none";
-    educationSection.style.display = "flex";
+    skillsSection.style.display = "none";
+    projectsSection.style.display = "none";
+    contactSection.style.display = "none";
+  } else if (btn.classList.contains("skills")) {
+    skillsSection.style.display = "flex";
+    homeSection.style.display = "none";
+    aboutSection.style.display = "none";
+    educationSection.style.display = "none";
+    projectsSection.style.display = "none";
+    contactSection.style.display = "none";
+  } else if (btn.classList.contains("projects")) {
+    projectsSection.style.display = "flex";
+    homeSection.style.display = "none";
+    aboutSection.style.display = "none";
+    educationSection.style.display = "none";
+    projectsSection.style.display = "none";
+    contactSection.style.display = "none";
+  } else if (btn.classList.contains("contact")) {
+    contactSection.style.display = "flex";
+    homeSection.style.display = "none";
+    aboutSection.style.display = "none";
+    educationSection.style.display = "none";
+    projectsSection.style.display = "none";
   }
 }
 //# sourceMappingURL=main.dev.js.map
--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const buttons = document.querySelectorAll(".button");
 const aboutSection = document.querySelector(".about_container");
 const homeSection = document.querySelector(".home_container");
 const educationSection = document.querySelector(".education_container");
+const skillsSection = document.querySelector(".skills__Created acontainer");
+const projectsSection = document.querySelector(".projects_container");
+const contactSection = document.querySelector(".contact")
+
 
 
 
@@ -14,14 +18,49 @@ function switched(e) {
     if(btn.classList.contains("home")) {
         homeSection.style.display = "grid";
         aboutSection.style.display = "none";
-        educationSection.style.display = "none"
+        educationSection.style.display = "none";
+        skillsSection.style.display = "none";
+        projectsSection.style.display = "none";
+        contactSection.style.display = "none";
+
     }else if(btn.classList.contains("about")) {
-        homeSection.style.display = "none";
         aboutSection.style.display = "block";
-        educationSection.style.display = "none"
+        homeSection.style.display = "none";
+        educationSection.style.display = "none";
+        skillsSection.style.display = "none";
+        projectsSection.style.display = "none";
+        contactSection.style.display = "none";
+
     }else if(btn.classList.contains("education")) {
+        educationSection.style.display = "flex";
         homeSection.style.display = "none";
         aboutSection.style.display = "none";
-        educationSection.style.display = "flex"
+        skillsSection.style.display = "none";
+        projectsSection.style.display = "none";
+        contactSection.style.display = "none";
+
+    }else if(btn.classList.contains("skills")) {
+        skillsSection.style.display = "flex";
+        homeSection.style.display = "none";
+        aboutSection.style.display = "none";
+        educationSection.style.display = "none";
+        projectsSection.style.display = "none";
+        contactSection.style.display = "none";
+
+
+    }else if(btn.classList.contains("projects")) {
+        projectsSection.style.display = "flex";
+        homeSection.style.display = "none";
+        aboutSection.style.display = "none";
+        educationSection.style.display = "none";
+        projectsSection.style.display = "none";
+        contactSection.style.display = "none";
+
+    }else if(btn.classList.contains("contact")) {
+        contactSection.style.display = "flex";
+        homeSection.style.display = "none";
+        aboutSection.style.display = "none";
+        educationSection.style.display = "none";
+        projectsSection.style.display = "none";
     }
 }
